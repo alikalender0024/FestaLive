84d3198e2ec3b38d92a742b7dca3fa0b363a88b7
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 14:53:23 +0300

    + Integrated Autofac for DI configuration and added

--- FestaLive.WebUI/Program.cs
+++ FestaLive.WebUI/Program.cs
@@ -1,15 +1,24 @@
+using Autofac.Extensions.DependencyInjection;
+using Autofac;
+using FestaLive.Business.DependencyResolvers.Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(builder =>
+       {
+           builder.RegisterModule(new AutofacBusinessModule());
+
+       });
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
