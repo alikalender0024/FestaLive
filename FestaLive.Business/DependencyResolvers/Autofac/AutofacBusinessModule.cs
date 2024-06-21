0a203ba91af01310a59dbacd01b9219aca354cd1
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 14:52:47 +0300

    + Integrated Autofac for DI configuration and added

---
+++ FestaLive.Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using FestaLive.Business.Abstract;
+using FestaLive.Business.Concrete;
+using FestaLive.DataAccess.Abstract;
+using FestaLive.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestaLive.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ArtistManager>().As<IArtistService>().SingleInstance();
+            builder.RegisterType<EfArtistDal>().As<IArtistDal>().SingleInstance();
+        }
+    }
+}
--- FestaLive.WebAPI/Program.cs
+++ FestaLive.WebAPI/Program.cs
@@ -1,15 +1,24 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using FestaLive.Business.DependencyResolvers.Autofac;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory())
+       .ConfigureContainer<ContainerBuilder>(builder =>
+       {
+           builder.RegisterModule(new AutofacBusinessModule());
+       });
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
