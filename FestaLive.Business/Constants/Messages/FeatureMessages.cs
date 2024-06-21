bc0189456a41285c95def2566ac0ad70167c6c3e
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 15:42:58 +0300

    + + Admin Layout Components Partially Separated

---
+++ FestaLive.Business/Constants/Messages/FeatureMessages.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestaLive.Business.Constants.Messages
+{
+    internal class FeatureMessages
+    {
+        public static string FeatureAddedSuccessfully => "Öne Çıkan başarıyla eklendi.";
+        public static string FeatureUpdatedSuccessfully => "Öne Çıkan başarıyla güncellendi.";
+        public static string FeatureDeletedSuccessfully => "Öne Çıkan başarıyla silindi.";
+
+        public static string FeatureNotFound => "Öne Çıkan bulunamadı.";
+        public static string InvalidFeatureId => "Geçersiz Öne Çıkan ID'si.";
+
+        public static string InvalidStartDate => "Geçersiz başlangıç tarihi.";
+        public static string InvalidEndDate => "Geçersiz bitiş tarihi.";
+
+        public static string FeatureListed => "Öne Çıkanlar başarıyla listelendi.";
+        public static string FeaturesNotFound => "Öne Çıkanlar bulunamadı.";
+
+        public static string FeatureValidationFailed => "Öne Çıkan doğrulaması başarısız oldu.";
+    }
+
+}
---
+++ FestaLive.WebUI/ViewComponents/AdminLayoutViewComponent/AdminLayoutScriptComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FestaLive.WebUI.ViewComponents.AdminLayoutViewComponent
+{
+    public class AdminLayoutScriptComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
