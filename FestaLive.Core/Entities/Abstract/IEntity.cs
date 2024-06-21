ead5d642d2721b5c08a9bbc86cb477c9a7e60708
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 14:07:35 +0300

    + Namespace Naming Fixed

--- FestaLive.Core/Entities/Abstract/IEntity.cs
+++ FestaLive.Core/Entities/Abstract/IEntity.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FestaLive.Core.DataAccess.Abstract
+namespace FestaLive.Core.Entities.Abstarct
 {
     public interface IEntity
     {
--- FestaLive.Entities/Concrete/Footer.cs
+++ FestaLive.Entities/Concrete/Footer.cs
@@ -1,4 +1,4 @@
-﻿using FestaLive.Core.DataAccess.Abstract;
+﻿using FestaLive.Core.Entities.Abstarct;
 using System;
 using System.Collections.Generic;
 using System.Linq;
