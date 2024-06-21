173f43ca255ff87fbdda4cfba47a2c4b1bda2dcc
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 14:37:53 +0300

    + Added Result structure for response

---
+++ FestaLive.Core/Utilities/Results/IResult.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestaLive.Core.Utilities.Results
+{
+    public interface IResult
+    {
+        string Message { get; }
+        bool IsSuccess { get; }
+    }
+}
