27769ac470664f1178ae9fc19355714b0c7f2867
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-21 14:45:48 +0300

    + Business Coding + ArtistManager Coded

---
+++ FestaLive.Business/Abstract/IArtistService.cs
@@ -0,0 +1,19 @@
+﻿using FestaLive.Core.Utilities.Results;
+using FestaLive.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FestaLive.Business.Abstract
+{
+    public interface IArtistService
+    {
+        IResult Add(Artist artist);
+        IResult Delete(Artist artist);
+        IResult Update(Artist artist);
+        IDataResult<Artist> GetById(int artistId);
+        IDataResult<List<Artist>> GetAll();
+    }
+}
---
+++ FestaLive.Business/Concrete/ArtistManager.cs
@@ -0,0 +1,53 @@
+﻿using FestaLive.Business.Abstract;
+using FestaLive.Core.Utilities.Results;
+using FestaLive.DataAccess.Abstract;
+using FestaLive.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace FestaLive.Business.Concrete
+{
+    public class ArtistManager : IArtistService
+    {
+        private readonly IArtistDal _artistDal;
+
+        public ArtistManager(IArtistDal artistDal)
+        {
+            _artistDal = artistDal;
+        }
+
+        public IResult Add(Artist artist)
+        {
+            _artistDal.Add(artist);
+            return new SuccessResult("Artist added successfully");
+        }
+
+        public IResult Delete(Artist artist)
+        {
+            _artistDal.Delete(artist);
+            return new SuccessResult("Artist deleted successfully");
+        }
+
+        public IDataResult<List<Artist>> GetAll()
+        {
+            var artists = _artistDal.GetAll();
+            return new SuccessDataResult<List<Artist>>(artists);
+        }
+
+        public IDataResult<Artist> GetById(int artistId)
+        {
+            var artist = _artistDal.Get(a => a.Id == artistId);
+            if (artist != null)
+            {
+                return new SuccessDataResult<Artist>(artist);
+            }
+            return new ErrorDataResult<Artist>("Artist not found");
+        }
+
+        public IResult Update(Artist artist)
+        {
+            _artistDal.Update(artist);
+            return new SuccessResult("Artist updated successfully");
+        }
+    }
+}
