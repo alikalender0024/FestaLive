5cdbb84bd2a367b9b9bbc9f082dd995e2d90f59c
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-24 02:55:15 +0300

    + ContactForm Completed + fixed

---
+++ FestaLive.WebUI/Models/ContactFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace FestaLive.WebUI.Models
+{
+    public class ContactFormViewModel
+    {
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Company { get; set; }
+        public string Message { get; set; }
+    }
+}
