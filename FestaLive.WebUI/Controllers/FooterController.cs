ba3bdb0af16507339324bb182a35563ac83b0cd6
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-29 18:25:44 +0300

    + Footer CRUD Operation Completed

--- FestaLive.WebUI/Controllers/FooterController.cs
+++ FestaLive.WebUI/Controllers/FooterController.cs
@@ -1,12 +1,68 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FestaLive.Business.Abstract;
+using FestaLive.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FestaLive.WebUI.Controllers
 {
     public class FooterController : Controller
     {
-        public IActionResult Index()
+        private readonly IFooterService _footerService;
+
+        public FooterController(IFooterService footerService)
+        {
+            _footerService = footerService;
+        }
+
+        public IActionResult FooterList()
+        {
+            var footers = _footerService.GetAll().Data;
+            return View(footers);
+        }
+
+        [HttpGet]
+        public IActionResult CreateFooter()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateFooter(Footer footer)
+        {
+            if (ModelState.IsValid)
+            {
+                _footerService.Add(footer);
+                return RedirectToAction("FooterList");
+            }
+
+            return View(footer);
+        }
+        public IActionResult DeleteFooter(int id)
+        {
+            _footerService.Delete(id);
+            return RedirectToAction("FooterList");
+        }
+        [HttpGet]
+        public IActionResult UpdateFooter(int id)
+        {
+            var footer = _footerService.GetById(id).Data;
+            if (footer == null)
+            {
+                return NotFound();
+            }
+
+            return View(footer);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateFooter(Footer footer)
+        {
+            if (ModelState.IsValid)
+            {
+                _footerService.Update(footer);
+                return RedirectToAction("FooterList");
+            }
+
+            return View(footer);
         }
     }
 }
