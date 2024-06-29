e9ee0917c572297041361d37135fdbe7aa3f322c
Muhammed Ali KALENDER <walikalender@gmail.com>
2024-06-29 19:04:33 +0300

    + SocialMedia CRUD Operation Completed

--- FestaLive.WebUI/Controllers/SocialMediaController.cs
+++ FestaLive.WebUI/Controllers/SocialMediaController.cs
@@ -1,12 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FestaLive.Business.Abstract;
+using FestaLive.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FestaLive.WebUI.Controllers
 {
-    public class SocialMediaController : Controller
+    public class SocialMediaController(ISocialMediaService socialMediaService) : Controller
     {
-        public IActionResult Index()
+        private readonly ISocialMediaService _socialMediaService = socialMediaService;
+
+        public IActionResult SocialMediaList()
+        {
+            var result = _socialMediaService.GetAll().Data;
+            return View(result);
+        }
+        public IActionResult CreateSocialMedia()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateSocialMedia(SocialMedia socialMedia)
+        {
+            _socialMediaService.Add(socialMedia);
+            return RedirectToAction("SocialMediaList");
+        }
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+            var socialMedia = _socialMediaService.GetById(id).Data;
+            return View(socialMedia);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            _socialMediaService.Update(socialMedia);
+            return RedirectToAction("SocialMediaList");
+        }
+
+        [HttpPost]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            _socialMediaService.Delete(id);
+            return RedirectToAction("SocialMediaList");
         }
     }
 }
